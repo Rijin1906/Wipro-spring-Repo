933daafeee71c44836f0f8807f93c95b6e6361df
Rijin Raj <75530710+Rijin1906@users.noreply.github.com>
2024-06-21 16:58:59 +0530

    Add files via upload

---
+++ producer-service/src/main/java/com/wipro/model/User.java
@@ -0,0 +1,23 @@
+package com.wipro.model;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+	private Integer userid;
+	private String name;
+	private String password;
+	private LocalDateTime loggedin;
+	
+	
+	public User() {
+		
+	}
+
+}
